67bd658a4714a60743b303e381f44911a09acc20
neeth <neeth@192.168.1.212>
2022-01-16 16:19:45 -0600

    New File added

---
+++ spring-mvc-demo/src/com/spring/demo/TestProject.java
@@ -0,0 +1,13 @@
+package com.spring.demo;
+
+public class TestProject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a= 3, b= 4;
+		int sum = a+b;
+		System.out.println(sum);
+
+	}
+
+}
