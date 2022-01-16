678fba1194b326dae3667ca42bad920737ff34f1
neethu87 <95923379+neethu87@users.noreply.github.com>
2022-01-16 17:59:16 -0600

    Update TestProject.java

--- spring-mvc-demo/src/com/spring/demo/TestProject.java
+++ spring-mvc-demo/src/com/spring/demo/TestProject.java
@@ -5,7 +5,7 @@ public class TestProject {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int a= 3, b= 5;
-		int c=6;
+		int c=600;
 		int sum = a+b;
 		System.out.println(sum);
 		System.out.println(c);
